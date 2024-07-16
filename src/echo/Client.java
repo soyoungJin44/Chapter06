edf63e9e2504dec9d7c5383b3909f6c354d99bdc
soyoungJin44 <wlsthdud0412@naver.com>
2024-07-16 11:48:19 +0900

    + scanner

--- src/echo/Client.java
+++ src/echo/Client.java
@@ -13,48 +13,52 @@ import java.util.Scanner;
 
 public class Client {
 
-			//연결할 다른 컴터라고 생각
+	// 연결할 다른 컴터라고 생각
 	public static void main(String[] args) throws IOException {
-		
-		//입력용 스캐너
+
+		// 입력용 스캐너
 		Scanner sc = new Scanner(System.in);
-		System.out.print("입력해주세요:");
-		String str = sc.nextLine();
-		
-		Socket socket = new Socket();		//전화기 들고 시작
+
+		Socket socket = new Socket(); // 전화기 들고 시작
 		System.out.println("클라이언트 시작");
-		
+
 		System.out.println("서버에 연결을 요청합니다.");
-		socket.connect(new InetSocketAddress("192.168.0.52", 10001));		//이 ip로 찾아가라
-		
+		socket.connect(new InetSocketAddress("192.168.0.52", 10001)); // 이 ip로 찾아가라
+
 		System.out.println("서버에 연결 되었습니다.");
-		
-		
-		//소켓이 주소트림을 만들어서 달라고 하면됨ㅇㅇ
-		OutputStream out= socket.getOutputStream();
-		OutputStreamWriter osr = new OutputStreamWriter(out,"UTF-8");
-		
+
+		// 소켓이 주소트림을 만들어서 달라고 하면됨ㅇㅇ
+		OutputStream out = socket.getOutputStream();
+		OutputStreamWriter osr = new OutputStreamWriter(out, "UTF-8");
+
 		BufferedWriter bw = new BufferedWriter(osr);
-		
+
 		InputStream in = socket.getInputStream();
-		InputStreamReader isr = new InputStreamReader(in,"UTF-8");
-		
+		InputStreamReader isr = new InputStreamReader(in, "UTF-8");
+
 		BufferedReader br = new BufferedReader(isr);
-		
-		
-		bw.write(str);
-		bw.newLine();
-		bw.flush();		//buffered 공간이 다 안차도 보내주세요.
-		
-		String msg1 = br.readLine();
-		System.out.println("돌려받은받은 메세지: " + msg1);
-		
-		System.out.println("===============");
-		System.out.println("끄으응읏");
+
+		while (true) {
+			System.out.print("입력해주세요:");
+			String str = sc.nextLine();
+
+			if ("/q".equals(str)) {
+				break;
+			}
+
+			bw.write(str);
+			bw.newLine();
+			bw.flush(); // buffered 공간이 다 안차도 보내주세요.
+
+			String msg1 = br.readLine();
+			System.out.println("돌려받은받은 메세지: " + msg1);
+		}
+			System.out.println("===============");
+			System.out.println("끄으응읏");
 		
 		bw.close();
 		br.close();
 		socket.close();
 	}
-	
+
 }
